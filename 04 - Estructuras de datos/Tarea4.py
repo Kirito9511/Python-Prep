a13a86cdf9453dc0ec1eda4ce4fa40cd20fd5c17
Kirito9511 <charnino@hotmail.com>
2022-12-03 12:59:37 -0500

    ok1

--- 04 - Estructuras de datos/Tarea4.py
+++ 04 - Estructuras de datos/Tarea4.py
@@ -61,15 +61,32 @@ print(20 in Tu)  #devuelve TRUE
 print(30 in Tu)  #devuelve FALSE
 
 #19)Con la lista creada en el punto 1, validar la existencia del elemento 'París' y si no existe, agregarlo. Utilizar una variable e informar lo sucedido.
+print(A)
+print(A.index("colombia"))
+palabra="belgica"
+if(not(palabra in A)):
+    print(palabra,"no esta")
+    A.append(palabra)
+    print(A)
+ 
+else:
+    print("esta en la posicion,",A.index(palabra))
 
 #20)Mostrar la cantidad de veces que se encuentra un elemento específico dentro de la tupla y de la lista
-
+print(A.count("colombia"))
+print(Tu.count(20))
 #21)Convertir la tupla en una lista
-
+Tu1=list(Tu)
+print(Tu1)
 #22)Desempaquetar solo los primeros 3 elementos de la tupla en 3 variables
-
+print(Tu1[0])
+print(Tu1[1])
+print(Tu1[2])
 #23)Crear un diccionario utilizando la lista crada en el punto 1, asignandole la clave "ciudad". Agregar tambien otras claves, como puede ser "Pais" y "Continente".
-
+dictionary={"ciudad": A,"pais":Tu,"Continente":Tu1}
+print(dictionary)
 #24)Imprimir las claves del diccionario
-
+print(dictionary.keys())
 #25)Imprimir las ciudades a través de su clave
+print(dictionary["Continente"])
+print(dictionary["ciudad"])
